6bee5cccf76cc33412bced59cfa4d02eb68d9445
ashtondscottnz <ashton.dscott.nz@gmail.com>
2026-06-09 08:39:22 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace basic_calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
